83752e5a539e4b5b0b04c24ce0ecbb20e2cecea0
isuruSandeepa <sandeepaisuru151@gmail.com>
2026-03-10 16:11:50 +0530

    create cart👌

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,19 @@ function App() {
   return (
     <>
       <div>
-        Isuru sandeepa
+        <div>
+            <h1>Mac book</h1>
+            <img src = "https://picsum.photos/seed/picsum/200/300" alt = "Default picture"/>
+            <p>LKR 150,000</p>
+            <button>Buy now</button>
+        </div>
+
+        <div>
+          <h1>Apple iphone</h1>
+            <img src = "https://picsum.photos/200/300?grayscale" alt = "Default picture"/>
+            <p>LKR 100,000</p>
+            <button>Buy now</button>
+        </div>
       </div>
       
     </>
